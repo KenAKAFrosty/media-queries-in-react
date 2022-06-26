69ffe20ca1d2fd049d989026aed9c224f2770c46
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-06-25 20:10:21 -0400

    add: declare module in d.ts

--- useMediaQueries.d.ts
+++ useMediaQueries.d.ts
@@ -10,5 +10,6 @@ export declare type QueriesMatches = {
 export default function useMediaQueries<T extends StringToString>(queriesByFriendlyNames: T): {
     [Property in keyof T]: boolean;
 };
+declare module 'media-queries-in-react'
 export {};
 //# sourceMappingURL=useMediaQueries.d.ts.map
